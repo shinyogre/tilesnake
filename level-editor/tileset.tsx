4cbea42c8917f7b9e7e08dbad2110f31536ba909
glenn <glenn@cooper.localdomain>
2018-05-05 11:31:57 -0400

    revised level editor

--- level-editor/tileset.tsx
+++ level-editor/tileset.tsx
@@ -1,4 +1,16 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="tileset" tilewidth="32" tileheight="32" tilecount="25" columns="5">
  <image source="tileset.png" width="160" height="160"/>
+ <tile id="2">
+  <objectgroup draworder="index">
+   <object id="5" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0">
+    <polyline points="0,0 32,0 32,32 0,32 0,0"/>
+   </object>
+  </objectgroup>
+ </tile>
 </tileset>
